7683df391a045e8e35f17025daccd687ff9e7b18
ISecka <ISecka@N-NO-01-01-0093>
2024-10-01 08:35:28 +0200

    Core done

--- src/components/AnswersItem.jsx
+++ src/components/AnswersItem.jsx
@@ -1,45 +1,38 @@
-// Components don't need to be separeted into individual files
-// Here we have a smaller component that helps compose the AnswersItem below
-
-const answersSet = {
-  swimming: "Swimming",
-  bathing: "Bathing",
-  chatting: "Chatting",
-  noTime: "I don't like to spend time with it"
-};
-
 function ItemsList({ list }) {
   return (
     <ul>
-      {list.map((item) => (
-        <li>{answersSet[item]}</li>
+      {list.map((item, index) => (
+        <li key={index}>{answersSet[item]}</li>
       ))}
     </ul>
   );
 }
 
-// This is the main component being exported from this file
-export default function AnswersItem({
-  // Feel free to change this props names to what suits you best
-  // Rememeber here we're destructuring answerItem, which is the prop name that we've passed
-  answerItem: { username, colour, timeSpent, review }
-}) {
+
+export default function AnswersItem({answerItem, setFormAnswers, setEditAnswer, index }) {
+   function handleEdit(event) {
+     console.log("answers Item right here", answerItem);
+     setFormAnswers(answerItem);
+     setEditAnswer(index)
+     console.log("editAnswer", index)
+   }
   return (
     <li>
       <article className="answer">
-        <h3>{username || "Anon"} said:</h3>
+        <h3>{answerItem.username || "Anon"} said:</h3>
         <p>
           <em>How do you rate your rubber duck colour?</em>
-          <span className="answer__line">{colour}</span>
+          <span className="answer__line">{answerItem.colour}</span>
         </p>
         <p>
           <em>How do you like to spend time with your rubber duck?</em>
-          <ItemsList list={timeSpent} />
+          <ItemsList list={answerItem.timeSpent} />
         </p>
         <p>
           <em>What else have you got to say about your rubber duck?</em>
-          <span className="answer__line">{review}</span>
+          <span className="answer__line">{answerItem.review}</span>
         </p>
+        <button id="edit-button" onClick={handleEdit}>Edit</button>
       </article>
     </li>
   );
